392721b182908de6a12de1eae87c0756c44a0806
Jens Maurer <Jens.Maurer@gmx.net>
2019-01-26 20:08:06 +0100

    Various bug fixes.

--- scripts/github-post.sh
+++ scripts/github-post.sh
@@ -6,6 +6,6 @@ path=$1
 url="https://api.github.com$path"
 echo "$url"
 
-curl -i --cacert cacert.pem -u jensmaurer:$token \
+curl -s -i --cacert cacert.pem -u jensmaurer:$token \
      -H "Accept: application/vnd.github.v3.raw+json" \
      --data @- "$url"
--- scripts/parse-papers.pl
+++ scripts/parse-papers.pl
@@ -26,9 +26,15 @@ my %groupnames =
 		   
      "SG1" => "SG1",
      "SG6" => "SG6",
+     "SG7" => "SG7",
 
+     "SG12" => "SG12",
+     "SG13" => "SG13",
+     "SG14" => "SG14",
      "SG15" => "SG15",
-     "SG16" => "SG16");
+     "SG16" => "SG16",
+     "SG19" => "SG19",
+     "SG20" => "SG20");
 
 
 foreach my $x (@p) {
@@ -60,70 +66,79 @@ foreach my $x (@p) {
 	    push @groups, $groupnames{$g};
 	} else {
 	    print STDERR "Cannot map group '$g' for $pnum $title\n";
-	    exit 1;
 	}
     }
 
     my $body = "[$pnum](https://wg21.link/$lcpnum) $title ($author)";
 
-    if ($rev > 0) {
-	# Look for an existing issue for this paper.
-	my $q = "$pseries is:issue in:title repo:$repo";
-	$q =~ s/ /+/g;
+    # Look for an existing issue for this paper.
+    my $q = "$pseries is:issue in:title repo:$repo";
+    $q =~ s/ /+/g;
 
+    local $/;
+    open(F, "./github-get.sh '/search/issues?q=$q'|") || die "cannot GET";
+    my $resp = <F>;
+    close F;
+    sleep 1;
+    my $obj = decode_json($resp);
+    if (exists $obj->{message} && exists $obj->{documentation_url}) {
+	# Some error; probably "API rate limit exceeded"
+	next;
+    }
+
+    # Make sure the title actually starts with the given paper number.
+    my $issue = undef;
+    my @i = ();
+    foreach my $i (@{$obj->{items}}) {
+	if ($i->{title} =~ /^$pseries /) {
+	    $issue = $i;
+	    push(@i, $i->{number});
+	}
+    }
+    if (@i > 1) {
+	print "Duplicate issues for $pseries: ", join(", ", @i), "\n";
+    }
+    if (defined $issue) {
+	my $number = $issue->{number};
+	    
+	print "Found #$number $issue->{title}\n";
+	# Skip paper if issue body already contains paper number.
+	next if $issue->{body} =~ /^\[$pnum/;
+	
+	# Step 1: Retrieve existing comments.
 	local $/;
-	open(F, "./github-get.sh '/search/issues?q=$q'|") || die "cannot GET";
-	my $resp = <F>;
+	open(F, "./github-get.sh /repos/$repo/issues/$number/comments|") || die "cannot GET comments";
+	my $comments = decode_json(<F>);
 	close F;
-	my $obj = decode_json($resp);
 
-	# Make sure the title actually starts with the given paper number.
-	my $issue = undef;
-	foreach my $i (@{$obj->{items}}) {
-	    if ($i->{title} =~ /^$pseries /) {
-		$issue = $i;
-		last;
-	    }
+	my $found = 0;
+	foreach my $c (@$comments) {
+	    $found = 1 if $c->{body} =~ /^\[$pnum/;
 	}
-	if (defined $issue) {
-	    print "Found  $issue->{title}\n";
-	    # Skip paper if issue body already contains paper number.
-	    next if $issue->{body} =~ /^\[$pnum/;
-	    
-	    my $number = $issue->{number};
-	    
-	    # Step 1: Retrieve existing comments.
-	    local $/;
-	    open(F, "./github-get.sh /repos/$repo/issues/$number/comments|") || die "cannot GET comments";
-	    my $comments = decode_json(<F>);
-	    close F;
+	# Skip paper if comment body already has paper number. 
+	next if $found;
 
-	    my $found = 0;
-	    foreach my $c (@$comments) {
-		$found = 1 if $c->{body} =~ /^\[$pnum/;
-	    }
-	    # Skip paper if comment body already has paper number. 
-	    next if $found;
+	print "Updating for $pnum\n";
 
-	    print "Updating for $pnum\n";
+	# Step 2: Create a comment with the new paper info.
+	open(F, "|./github-post.sh /repos/$repo/issues/$number/comments") || die "cannot POST comment";
+	print F "{\n";
+	print F "  \"body\": \"$body\"";
+	print F "}\n";
+	close F;
 
-	    # Step 2: Update the labels and the milestones
-	    open(F, "|./github-post.sh /repos/$repo/issues/$number") || die "cannot POST issue";
-	    print F "{\n";
-	    print F "  \"labels\": [ ", join(",", map "\"$_\"", @groups), " ],\n";
-	    print F "  \"milestone\": $milestone\n";
-	    print F "}\n";
-	    close F;
+	# Do not change labels or milestones for closed issues.
+	next if ($issue->{state} eq "closed");
+	
+	# Step 3: Update the labels and the milestones
+	open(F, "|./github-post.sh /repos/$repo/issues/$number") || die "cannot POST issue";
+	print F "{\n";
+	print F "  \"labels\": [ ", join(",", map "\"$_\"", @groups), " ],\n";
+	print F "  \"milestone\": $milestone\n";
+	print F "}\n";
+	close F;
 
-	    # Step 3: Create a comment with the new paper info.
-	    open(F, "|./github-post.sh /repos/$repo/issues/$number/comments") || die "cannot POST comment";
-	    print F "{\n";
-	    print F "  \"body\": \"$body\"";
-	    print F "}\n";
-	    close F;
-	    
-	    next;
-	}
+	next;
     }
 
     if (!defined $reqpaper && @groups == 0) {
@@ -133,7 +148,9 @@ foreach my $x (@p) {
 
     # create new issue
     print "Creating $pseries $title\n";
-    open(F, "|./github-post.sh /repos/$repo/issues") || die "cannot POST new issue";
+    $title =~ s/"/\\"/g;    # escape quotation marks
+    $body =~ s/"/\\"/g;    # escape quotation marks
+    open(F, "|./github-post.sh /repos/$repo/issues | grep message") || die "cannot POST new issue";
     print F "{\n";
     print F "  \"title\": \"$pseries $title\",\n";
     print F "  \"body\": \"$body\",\n";
