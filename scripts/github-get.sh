24342298b1e602dc841238e5d394211f52537961
Jens Maurer <Jens.Maurer@gmx.net>
2019-02-12 21:25:59 +0100

    reworked format for file containing github access token

--- scripts/github-get.sh
+++ scripts/github-get.sh
@@ -1,17 +1,12 @@
 #!/bin/sh
 
-token=`cat ../../.github-access-token`
+. ../../.github-access-token
 
 path=$1
 url="https://api.github.com$path"
 
-mediatype="application/vnd.github.v3.raw+json"
-#mediatype="application/vnd.github.inertia-preview+json"
-#mediatype="application/vnd.github.mockingbird-preview"   # timeline
-#mediatype="application/vnd.github.starfox-preview+json"
-
-curl -s --cacert cacert.pem -u jensmaurer:$token \
-     -H "Accept: $mediatype" \
+curl -s --cacert cacert.pem -u "$GITHUB_AUTH" \
+     -H "Accept: application/vnd.github.v3.raw+json" \
      -H "Accept: application/vnd.github.mockingbird-preview" \
      -H "Accept: application/vnd.github.inertia-preview+json" \
      -H "Accept: application/vnd.github.starfox-preview+json" \
--- scripts/github-post.sh
+++ scripts/github-post.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-token=`cat ../../.github-access-token`
+. ../../.github-access-token
 
 path=$1
 url="https://api.github.com$path"
-echo "$url"
 
-curl -s -i --cacert cacert.pem -u jensmaurer:$token \
+curl -s -i --cacert cacert.pem -u $GITHUB_AUTH \
      -H "Accept: application/vnd.github.v3.raw+json" \
      --data @- "$url"
