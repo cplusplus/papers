15f24729992b67760ba8f9e4ef107494c8625d4a
Jens Maurer <Jens.Maurer@gmx.net>
2020-04-24 19:13:26 +0200

    Add script to send DELETE request to github API.

---
+++ scripts/github-delete.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+. ../../.github-access-token
+
+path=$1
+url="https://api.github.com$path"
+
+curl -s -i --cacert cacert.pem -u $GITHUB_AUTH \
+     -H "Accept: application/vnd.github.v3.raw+json" \
+     -X DELETE "$url"
